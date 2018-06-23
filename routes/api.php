9340cd05a039d55980f83939f40512adfcfcbf8e
rajendra4503 <rajendra.singh450@gmail.com>
2018-06-23 17:05:54 +0530

    create model and controller of product and review

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,12 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::apiResource('/products','ProductController');
+
+Route::group(['prefix' => 'products'], function () {
+    Route::apiResource('/{products}/reviews','ReviewController');
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
